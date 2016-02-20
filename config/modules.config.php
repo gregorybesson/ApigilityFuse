534961e1277cdad4ae7bd554ec73d164caf447f8
gregorybesson <gregory.besson@adfab.fr>
2016-02-20 23:10:55 +0100

    first commit

---
+++ config/modules.config.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+/**
+ * List of enabled modules for this application.
+ */
+return array(
+    'Application',
+    'ZF\DevelopmentMode',
+    'ZF\Apigility',
+    'ZF\Apigility\Provider',
+    'ZF\Apigility\Documentation',
+    'AssetManager',
+    'ZF\ApiProblem',
+    'ZF\Configuration',
+    'ZF\OAuth2',
+    'ZF\MvcAuth',
+    'ZF\Hal',
+    'ZF\ContentNegotiation',
+    'ZF\ContentValidation',
+    'ZF\Rest',
+    'ZF\Rpc',
+    'ZF\Versioning',
+    'DoctrineModule',
+    'DoctrineORMModule',
+    'Phpro\DoctrineHydrationModule',
+    'ZF\Apigility\Doctrine\Server',
+    'ZF\Apigility\Documentation\ApiBlueprint',
+    'ZF\OAuth2\Doctrine',
+    'ZfcBase',
+    'ZfcUser',
+    'ZfcUserDoctrineORM',
+);
